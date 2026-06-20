c6c48b10813e32cfd889a6b8a1a5c7e1cd362ca6
Sayed Herzallah <sayedherzallah99@gmail.com>
2026-06-20 19:18:31 +0300

    change ip server back end

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,3 @@
-export const REST_BASE_URL = 'http://192.168.100.9:8000/api/v1';
+export const REST_BASE_URL = 'http://127.0.0.1:8000/api/v1';
 export const WS_BASE_URL = 'ws://192.168.100.9:8000/ws/dashboard/';
+
